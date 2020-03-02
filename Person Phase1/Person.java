d24fdc852f4f55a104c62984e4bf4d338c40075d
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-02 13:54:48 -0500

    Update Person.java

--- Person Phase1/Person.java
+++ Person Phase1/Person.java
@@ -1,25 +1,27 @@
 /**
- Create class Person without main method
-*/
+ * Phase 1 - Create Person class without main method
+ * @author benwiersma
+ * CMSC203
+ */
 public class Person {
- 
- /**
-  Add two fields:
- */
- String name;
- int age;
 
- /**
-  Prints name and age to console
- */
- public void SayName() {
-  System.out.println("My name is " + name + " and my age is " + age);
- }
+	/**
+	 * Add two fields
+	 */
+	String name;
+	int age;
 
- /**
-  Increments age by 1
- */
- public void HaveBirthday() {
-  age++;
- }
-} 
+	/**
+	 * Print name and age
+	 */
+	public void SayName() {
+		System.out.println("My name is " + name + " and my age is " + age);
+	}
+
+	/**
+	 * Increment age by 1
+	 */
+	public void HaveBirthday() {
+		age++;
+	}
+}

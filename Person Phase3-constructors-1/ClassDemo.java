84de968b804f39903f1cf048115d5747cf77c3f1
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-02 14:13:51 -0500

    Update ClassDemo.java

--- Person Phase3-constructors-1/ClassDemo.java
+++ Person Phase3-constructors-1/ClassDemo.java
@@ -1,6 +1,15 @@
-
+/**
+ * Phase 3 - Added the use of constructors to initialize objects with data passed by programmer
+ * @author benwiersma
+ * CMSC203
+ */
 public class ClassDemo {
 	public static void main(String[] args) {
+		
+		/**
+		 * Create object with no-arg constructor
+		 * Sets fields to default initial values
+		 */
 		Person a = new Person();
 		
 		System.out.println(a.getName());
@@ -12,17 +21,28 @@ public class ClassDemo {
 		a.setAge(21);
 		
 		System.out.println(a.getAge());	
-		// create object named b with name Kim
+
+		/**
+		 * Create object with parameterized constructor
+		 * Passing string to name field
+		 */
 		Person b = new Person("Kim");
 		System.out.println(b.getName());
 		System.out.println(b.getAge());	
 		
+		/**
+		 * Create object with parameterized constructor
+		 * Passing string and int to respective fields
+		 */
 		Person c = new Person("John", 40);
 		System.out.println(c.getName());
 		System.out.println(c.getAge());
 		
-		//Create another person object d, 
-		//ask for user to select name and age
+		// Task for Student
+		/**
+		 * Create another Person object d using user input
+		 * Print to screen
+		 */
 	
 	}
 }

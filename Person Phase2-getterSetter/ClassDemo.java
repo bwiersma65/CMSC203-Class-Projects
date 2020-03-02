d24f82b06024fb33f807eb5f5062ee0da2f4e955
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-02 13:51:59 -0500

    Added javadoc comments

--- Person Phase2-getterSetter/ClassDemo.java
+++ Person Phase2-getterSetter/ClassDemo.java
@@ -1,9 +1,20 @@
+/**
+ Phase 2 - Changed to utilize getter and setter methods to access private fields
+*/
 public class ClassDemo {
+	
 	public static void main(String[] args) {
+		
 		Person a = new Person();
 		
+		/**
+		 Call setter to set name field to "Bob"
+		*/
 		a.setName("Bob");
 		
+		/**
+		 Call getter to print name to screen
+		*/
 		System.out.println(a.getName());
 		
 		a.setAge(21);

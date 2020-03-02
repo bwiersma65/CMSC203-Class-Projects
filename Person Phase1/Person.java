2e988226e62fc12c5909a050aa4bfcf42ecdf3ce
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-02 13:47:07 -0500

    Added Javadoc comments

--- Person Phase1/Person.java
+++ Person Phase1/Person.java
@@ -1,18 +1,25 @@
 /**
-
+ Create class Person without main method
 */
 public class Person {
+ 
+ /**
+  Add two fields:
+ */
  String name;
  int age;
-/**
 
-*/
-public void SayName(){
- System.out.println("My name is " + name + " and my age is " + age);
-}
-/**
-*/
-public void HaveBirthday() {
- age++;
-}
-}
+ /**
+  Prints name and age to console
+ */
+ public void SayName() {
+  System.out.println("My name is " + name + " and my age is " + age);
+ }
+
+ /**
+  Increments age by 1
+ */
+ public void HaveBirthday() {
+  age++;
+ }
+} 

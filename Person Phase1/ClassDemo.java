89e4730e8d795428764f0cc6f5afbbe294f556cf
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-02 13:39:57 -0500

    Update ClassDemo.java

--- Person Phase1/ClassDemo.java
+++ Person Phase1/ClassDemo.java
@@ -1,15 +1,22 @@
 public class ClassDemo {
  public static void main(String[] args) {
+ // create Person object a
  Person a = new Person();  
+  // Assign string to name field
   a.name = "Bob"; 
+  // Assign int to age field
   a.age = 25;  
+ // create another Person object b
  Person b = new Person();  
   b.name = "Mary"; 
   b.age = 35;  
  a.SayName(); 
+ // increments age of a by 1
  a.HaveBirthday();  
  b.SayName(); 
+ // increments age of b by 1
  b.HaveBirthday();  
+ // age should now be different from that displayed due to line 13 / 16
  a.SayName(); 
  b.SayName(); 
  }

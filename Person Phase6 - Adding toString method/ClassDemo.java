29764231836b22751edbd2447700f3b23dfe6d92
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-04 13:54:33 -0500

    Update ClassDemo.java

--- Person Phase6 - Adding toString method/ClassDemo.java
+++ Person Phase6 - Adding toString method/ClassDemo.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
 
+/**
+ * Phase 6 - toString method used
+ * @author benwiersma
+ * CMSC203
+ */
 public class ClassDemo {
 	public static void main(String[] args) {
  
 		
-		//Add code to create an object repeatedly
 		
 		String name,ans;
 		int age;
 		Person p;
+		
 		Scanner input = new Scanner(System.in);
 		do {
 
 			System.out.println("Enter name :");
 			name = input.nextLine();
+			
 			System.out.println("Enter age :");
 			age = input.nextInt();
+			
 			p = new Person(name,age);
-			System.out.println(p);// toString is called automatically here
+			
+			/**
+			 * In passing object p, the class toString method is implicitly invoked
+			 */
+			System.out.println(p);
+			
 			System.out.println(p.getName() + 
-					   " You are " +p.getAge() + " Years old");
+					   			" You are " +p.getAge() + " Years old");
+
 			input.nextLine();
+			
 			System.out.println("Do you want to continue?");
 			ans = input.nextLine();
 		}while (ans.equals("y") );

fff3fdc786c62c340195f5135d7f80fe053f59fa
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-04 13:43:53 -0500

    Update ClassDemo.java

--- Person Phase5-CreatingObject_Repeatedly/ClassDemo.java
+++ Person Phase5-CreatingObject_Repeatedly/ClassDemo.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 
+/**
+ * Phase 5 - Uses a do-while loop to enable creation of multiple objects
+ * @author benwiersma
+ * CMSC203
+ */
 public class ClassDemo {
 	public static void main(String[] args) {
  
-		
-		//Add code to create an object repeatedly
+		/**
+		 * Revised code to enable user to create new Person object and repeat as desired
+		 */
 		
 		String name,ans;
 		int age;
 		Person p;
+		
 		Scanner input = new Scanner(System.in);
 		do {
 
 			System.out.println("Enter name :");
 			name = input.nextLine();
+			
 			System.out.println("Enter age :");
 			age = input.nextInt();
+			
 			p = new Person(name,age);
+			
 			System.out.println(p.getName() + 
-					   " You are " +p.getAge() + " Years old");
+					   			" You are " +p.getAge() + " Years old");
+			// Read in next line stored in keyboard buffer to prevent erroneous reading
 			input.nextLine();
+			
 			System.out.println("Do you want to continue?");
 			ans = input.nextLine();
 		}while (ans.equals("y") );

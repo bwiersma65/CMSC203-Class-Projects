7470373c98bc57bab73b71ba8692ddde5f59b28f
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-04 15:05:00 -0500

    Update ClassDemo.java

--- Person Phase 10- Adding Array Of Cities in Peson Class/ClassDemo.java
+++ Person Phase 10- Adding Array Of Cities in Peson Class/ClassDemo.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
 
-import javax.swing.JOptionPane;
 /**
+ * Phase 10 - Added query prompting for cities person lived in
+ * @author benwiersma
  *
  */
 public class ClassDemo {
 	public static void main(String[] args) {
 	
 	 
-	 
 		String name,ans, city;
 		int age, size, cityCount=0;
 		double minWage;
 		Person p;
-		
 		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Enter name :");
 		name = input.nextLine();
 		
 		System.out.println("Enter age :");
 		age = input.nextInt();
+		
 		System.out.println("Enter min Wage :");
 		minWage  = input.nextInt();
-		input.nextLine();   //Skip the newLine
 		
-		p = new Person(name,age,minWage);  // Create the person
-		size = p.getDefaultSize();    //get the default size of the array of cities
+		input.nextLine();
 		
-		//Loop while the answer is y and the 
+		p = new Person(name,age,minWage);
+		size = p.getDefaultSize();
+		
 		do 
 		{
 			System.out.println("Enter the name of the city:");
 			city = input.nextLine();
+			/**
+			 * Invokes method to add city to array
+			 */
 			p.addCity(city);  
 			
 			System.out.println("Do you want to add another city: (y/n)");
@@ -39,13 +43,16 @@ public class ClassDemo {
 			cityCount++;
 			 
 			
+		/**
+		 * Loops while user answers yes OR number of cities matches capacity of array
+		 */
 		} while (cityCount<size && ans.equals("y") );
 		
 		
 		System.out.println("Here is the list of cities this person has lived in:");	
 		
-		System.out.println(p);   //Show the information of the person
-		System.out.println(p.citiesLivedIn());  //Show the information of the cities
+		System.out.println(p);
+		System.out.println(p.citiesLivedIn());
 		
 		
 		input.close();

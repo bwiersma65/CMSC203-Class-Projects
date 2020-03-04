d08c28437966651cf8dae0e3cd1e9aa3dbd8403c
bwiersma65 <55202800+bwiersma65@users.noreply.github.com>
2020-03-04 15:00:38 -0500

    Update Person.java

--- Person Phase 10- Adding Array Of Cities in Peson Class/Person.java
+++ Person Phase 10- Adding Array Of Cities in Peson Class/Person.java
@@ -4,20 +4,23 @@
  * @author KVAHABZ1
  *
  */
-
+/**
+ * Phase 10 - Added array of cities
+ * @author benwiersma
+ * CMSC203
+ */
 public class Person {
 	
 	private String name;
 	private int age;
 	private double minWage;
-	private String [] citiesLivedIn;  //array to keep the cities that this person has lived in
-	private int numOfCities;  //Keeps track of the number of cities in the citiesLivedIn.
-	private int DEFAULT_SIZE=  5;  // default size of the  citiesLivedIn;
+	private String [] citiesLivedIn;
+	private int numOfCities;
+	private int DEFAULT_SIZE=  5;
 
+	
 	/**
-	 * no-arg constructor, creates a Person
-	 * object with noname , age of 1 and minWage of 10
-	 * the citiesLivedIn with the default size is created
+	 * no-arg initializes String array
 	 */
 	public Person()
 	{
@@ -27,12 +30,7 @@ public class Person {
 		citiesLivedIn = new String[DEFAULT_SIZE];
 	}
 	 
-	/**
-	 * parameterized constructor, creates a Person
-	 * object with a name , age of 1 and minWage of 10
-	 * the citiesLivedIn with the default size is created
-	 * @param name name of the person
-	 */
+	
 	public Person(String name)
 	{
 		this.name = name;
@@ -41,14 +39,7 @@ public class Person {
 		citiesLivedIn = new String[DEFAULT_SIZE];
 	}
  
-	/**
-	 * parameterized constructor, creates a Person
-	 * object with a name , age  and minWage  
-	 * the citiesLivedIn with the default size is created
-	 * @param name name of the person
-	 * @param age person's age
-	 * @param minWage minimum wage
-	 */
+	
 	public Person(String name, int age, double minWage)
 	{
 		this.name = name;
@@ -57,18 +48,11 @@ public class Person {
 		citiesLivedIn = new String[DEFAULT_SIZE];
 	}
 	
-	/**
-	 * sets the name of the person
-	 * @param name the name of the person
-	 */
+	
 	public void setName(String name)
 	{
 		this.name = name;
 	}
-	/**
-	 * gets  the name of the person
-	 * returns the person's name
-	 */
 	public String getName()
 	{
 		return name;
@@ -82,6 +66,7 @@ public class Person {
 	{
 		return age;
 	}
+	
 	public void setWage(double minWage)
 	{
 		this.minWage = minWage;
@@ -91,18 +76,14 @@ public class Person {
 		return minWage;
 	}
 	
-	/**
-	 * returns the default size of the array
-	 * @return  default size of the array citiesLivedin
-	 */
 	public int getDefaultSize()
 	{
 		return DEFAULT_SIZE;
 	}
 	
 	/**
-	 * returns the array of citiesLivedin
-	 * @return  the array of citiesLivedIn
+	 * New - returns String array of cities
+	 * @return
 	 */
 	public String[] getcitiesLivedIn()
 	{
@@ -111,23 +92,25 @@ public class Person {
 		 * Another option would be to return the copy of 
 		 * the array to avoid another class to change the 
 		 * content of the citiesLivedIn array
+		 * 
+		 * A.K.A. Deep copy
 		 */
+	 	
 		 /*
-		   
-		
-		String [] tempCities = new String[citiesLivedIn.length];   //create a another array that has the same size of the original array
-		for ( int i = 0; i<citiesLivedIn.length; i++ )  //copy the content to this array
+		 String [] tempCities = new String[citiesLivedIn.length];   //create a another array that has the same size of the original array
+		 for ( int i = 0; i<citiesLivedIn.length; i++ )  //copy the content to this array
 			  
-		{
-			tempCities[i] = citiesLivedIn[i];
-		}
-		return 	tempCities;
-		*/
+		 {
+			 tempCities[i] = citiesLivedIn[i];
+		 }
+		 return 	tempCities;
+		 */
 	  
 	}
-	/*
-	 * Adds the city to the list of citiesLivedIn array
-	 * @param city name of the city to be added to the array
+	
+	/**
+	 * Adds city to array, increments counter
+	 * @param city
 	 */
 	public void addCity(String city)
 	{
@@ -135,45 +118,30 @@ public class Person {
 		numOfCities++;
 	}
 	
-	/**
-	 * displays the name and the age of the person
-	 * 
-	 */
+	
 	public void SayName() {
 		System.out.println("My name is " + name + " and my age "
 							+ "is " + age);
 	}
-	/**
-	 * increments age of the person
-	 * @param name the name of the person
-	 */
+	
 	public void HaveBirthday() {
 		age++;
 	}
 	
-	/**
-	 * increases the minWage by a percentage 
-	 * value passed as parameter to this method
-	 * @param perc the percentage value
-	 */
 	public void increaseWage(double perc) {
 		minWage = (minWage * perc)/100 + minWage;
 	}
 	
-	/**
-	 * toString method returns a String 
-	 * representing a Person object
-	 * @return returns String representation of object
-	 */
+	
 	public String toString()
 	{
 		return "The person's name is " + 
 				name + " and their age is " + age;
 	}
+	
 	/**
-	 * citiesLivedIn method returns a String 
-	 * representing list of cities 
-	 * @return returns String representation of cities in 
+	 * Returns String of cities lived in
+	 * @return all cities lived in
 	 */
 	public String citiesLivedIn()
 	{
